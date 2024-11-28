7631a11fcaa640c46e7a7f2389524459184bc108
manoj <devops_practice@test.com>
2024-11-28 20:24:43 +0530

    commit - 4

--- db/CreateEmployee.sql
+++ db/CreateEmployee.sql
@@ -1,6 +1,10 @@
-create table Employee(empName varchar(256) primary key);
-insert into Employee values ('701201');
-insert into Employee values ('701202');
-insert into Employee values ('701205');
-insert into Employee values ('701207');
-insert into Employee values ('701208');
+create table Employee(empName varchar(256) ,empId varchar(256) primary key);
+--alter table Employee add column empId varchar(256);
+insert into Employee values ('Manoj','701201');
+insert into Employee values ('kiran','701202');
+insert into Employee values ('uday','701205');
+insert into Employee values ('rahul','701207');
+insert into Employee values ('vijay','701208');
+
+--Roll Back Script
+drop table Employee;
